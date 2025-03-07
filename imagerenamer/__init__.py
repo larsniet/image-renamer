6c58d70cb3e584d4e804d00e795d891131463941
Lars van der Niet <lvdnbusiness@icloud.com>
2025-03-07 13:19:14 +0100

    Bump version to 1.0.26

--- imagerenamer/__init__.py
+++ imagerenamer/__init__.py
@@ -2,7 +2,7 @@
 Image Renamer - Rename image files based on their creation date from EXIF metadata.
 """
 
-__version__ = "1.0.25"
+__version__ = "1.0.26"
 __author__ = "Lars van der Niet"
 __license__ = "MIT"
 __url__ = "https://github.com/larsniet/image-renamer"
